fbb1d923f8fca7fe0d39d93b6c24bdde8606d4ad
stuxcrystal <stuxcrystal@gmail.com>
2015-02-13 02:36:26 -0500

    Lol... I love empty files.

---
+++ pykaraoke/templater.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# -*- encoding: utf-8 -*-
+from collections import namedtuple
+from functools import wraps
+import inspect
+__author__ = "stux!"
+
+Template = namedtuple("Template", "type func settings")
+
+
+class BaseTemplater(object):
+	"""
+	The basic templater class.
+	"""
+	
+	def __init__(self, *, fps=24000/1001, resolution=(1280,720)):
+		self.fps = fps
+		self.resolution = resolution
+		
+		self.types = {}
+		self.templates = []
+		
+	def _register_template(self, type: str, func: callable, args: tuple, settings: dict) -> None:
+		"""
+		Registers a new template.
+		"""
+		if type not in self.types:
+			raise ValueError("Unknown template type.")
+		type = self.types[type]
+		data = type.parse_attributes(args, settings)
+		self.templates.append(Template(type, func, data))
+		
+	def __call__(self, type: str, *args, **kwargs) -> callable:
+		"""
+		Registers a new template
+		"""
+		def _decorator(func):
+			# Make sure we get a generator as the templater
+			# will be iterate over the template executors.
+			if inspect.isgeneratorfunction(func):
+				_wrapper = func
+			else:
+				@wraps(func)
+				def _wrapper(*fargs, *fkwargs):
+					res = func(*fargs, *fkwargs)
+					if inspect.isgenerator(res):
+						res = yield from res
+					return res
+			self._register_template(type, _wrapper, args, kwargs)
+			return func
+		return _decorator
+	add = __call__
+	
+	
