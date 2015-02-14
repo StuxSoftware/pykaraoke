5d7805dac0f78f4b96c1092bec52e5921a38168f
stux <stuxcrystal@gmail.com>
2015-02-14 11:31:03 +0100

    Use actual namedtuple implementation.

--- pykaraoke/document.py
+++ pykaraoke/document.py
@@ -1,16 +1,57 @@
 #! /usr/bin/env python3
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
+from operator import itemgetter
 
 
-class Time(namedtuple("_Time", "start end")):
-    """
-    Defines the time in a pythonic manner.
-    """
+class Time(tuple):
+    """Time(start, end)'"""
 
     __slots__ = ()
 
-    def __init__(self, *args, **kwargs):
-        super(Time, self).__init__(*args, **kwargs)
+    _fields = ('start', 'end')
+
+    def __new__(cls, start, end):
+        """Create new instance of Time(start, end)'"""
+        return tuple.__new__(cls, (start, end))
+
+    @classmethod
+    def _make(cls, iterable, new=tuple.__new__, len=len):
+        """Make a new Time object from a sequence or iterable'"""
+        result = new(cls, iterable)
+        if len(result) != 2:
+            raise TypeError('Expected 2 arguments, got %d' % len(result))
+        return result
+
+    def _replace(self, **kwds):
+        """Return a new Time object replacing specified fields with new values'"""
+        result = self._make(map(kwds.pop, ('start', 'end'), self))
+        if kwds:
+            raise ValueError('Got unexpected field names: %r' % list(kwds))
+        return result
+
+    def __repr__(self):
+        """Return a nicely formatted representation string'"""
+        return self.__class__.__name__ + '(start=%r, end=%r)' % self
+
+    @property
+    def __dict__(self):
+        """A new OrderedDict mapping field names to their values'"""
+        return OrderedDict(zip(self._fields, self))
+
+    def _asdict(self):
+        """Return a new OrderedDict which maps field names to their values.'"""
+        return self.__dict__
+
+    def __getnewargs__(self):
+        """Return self as a plain tuple.  Used by copy and pickle.'"""
+        return tuple(self)
+
+    def __getstate__(self):
+        """Exclude the OrderedDict from pickling'"""
+        return None
+
+    start = property(itemgetter(0), doc='Alias for field number 0')
+    end = property(itemgetter(1), doc='Alias for field number 1')
 
     def retime(self, *, start: int=0, end: int=0) -> object:
         """
